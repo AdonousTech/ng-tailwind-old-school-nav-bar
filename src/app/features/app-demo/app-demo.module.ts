42480a92196d75fd267c2200fb24406ced548478
Matt Pitts <mattapitts@gmail.com>
2021-02-17 11:53:31 -0800

    feat (app demo): Add definition for AppDemoModule

--- src/app/features/app-demo/app-demo.module.ts
+++ src/app/features/app-demo/app-demo.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppDemoComponent } from './app-demo/app-demo.component';
-
+import { AppDemoComponent } from './app-demo.component';
+import { CustomComponentsModule } from '../../shared/custom-components';
 
 
 @NgModule({
   declarations: [AppDemoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CustomComponentsModule
   ]
 })
 export class AppDemoModule { }
