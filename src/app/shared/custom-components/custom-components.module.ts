5ec31a8d59517a44efdc4f3afc22988c5ea5e27f
Matt Pitts <mattapitts@gmail.com>
2021-02-17 11:46:49 -0800

    feat (custom components): Add CustomComponentsModuleDefinition

--- src/app/shared/custom-components/custom-components.module.ts
+++ src/app/shared/custom-components/custom-components.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconNavBarModule } from '../components/icon-nav-bar';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    IconNavBarModule
+  ],
+  exports: [
+    IconNavBarModule
   ]
 })
 export class CustomComponentsModule { }
