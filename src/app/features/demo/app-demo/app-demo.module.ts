04b6e8a9edf0530e771ff1bf75cd92f1dbb8180a
Matt Pitts <mattapitts@gmail.com>
2021-02-17 11:35:46 -0800

    feat (demo): Add AppDemoComponent and associated files @ features > app-demo root

--- src/app/features/demo/app-demo/app-demo.module.ts
+++ src/app/features/demo/app-demo/app-demo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppDemoComponent } from './app-demo/app-demo.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AppDemoComponent],
   imports: [
     CommonModule
   ]
