4f199ca88f88091bc1e674e0f43d0a82d0bdb986
Matt Pitts <mattapitts@gmail.com>
2021-02-17 12:26:22 -0800

    feat (demo): Add demo routing module @ features > demo > component-demo

--- src/app/features/demo/component-demo/component-demo.module.ts
+++ src/app/features/demo/component-demo/component-demo.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentDemoComponent } from './component-demo.component';
+import { ComponentDemoRoutingModule } from './component-demo-routing.module';
 import { CustomComponentsModule } from '../../../shared/custom-components';
 
 
@@ -9,7 +10,8 @@ import { CustomComponentsModule } from '../../../shared/custom-components';
   declarations: [ComponentDemoComponent],
   imports: [
     CommonModule,
-    CustomComponentsModule
+    CustomComponentsModule,
+    ComponentDemoRoutingModule
   ]
 })
 export class ComponentDemoModule { }
